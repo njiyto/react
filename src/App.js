806af758b7779f754fb7d2d7dbb77c140c73576e
Sergei Glybenko <njiyto@mail.ru>
2021-03-25 14:11:11 +0300

    add router, absolute import

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import {Home} from 'routes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+  <Router>
+    <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+
+      <hr />
+    <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  </div>
+);
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Home = () => {
+  return (
+    <div>
+      home page
+    </div>
+  )
+}
+
+export default Home;
